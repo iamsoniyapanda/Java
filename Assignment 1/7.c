41eaf044cd64267e4f3286f76ba5c893a0660918
SONIYA PANDA_MCA_1A_47_UEMK_ 2024-2026 <pandasoniya121@gmail.com>
2025-01-18 00:37:05 +0530

    Create 7.c

---
+++ Assignment 1/7.c
@@ -0,0 +1,20 @@
+//Write a Java Program to display whether a number is odd or even
+import java.util.Scanner;
+
+public class OddEvenChecker {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+        scanner.close();
+    }
+}

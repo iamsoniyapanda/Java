de49bc7af0848a5e06ed87356129b984bae65d52
SONIYA PANDA_MCA_1A_47_UEMK_ 2024-2026 <pandasoniya121@gmail.com>
2025-01-18 00:22:15 +0530

    Create 2.java

---
+++ Assignment 1/2.java
@@ -0,0 +1,10 @@
+//Write a Java Program to add two numbers by declaring variables value.
+public class Add
+  {
+    public static void main(String []args){
+      int num1=10;
+      int num2=30;
+      sum =num1+num2;
+      System.out.printn("sum");
+    }
+  }
